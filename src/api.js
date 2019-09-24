0acc4ef53afa0c93d91687e755c4f6f462010f2b
OlushkaZ <gincgo@yandex.ru>
2019-09-24 20:15:07 +0300

    module 9

---
+++ src/api.js
@@ -0,0 +1,75 @@
+import {ModelDestination} from './models/model-destination.js';
+import {ModelPoint} from './models/model-point.js';
+const Method = {
+  GET: `GET`,
+  POST: `POST`,
+  PUT: `PUT`,
+  DELETE: `DELETE`
+};
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+const toJSON = (response) => {
+  return response.json();
+};
+
+
+export const API = class {
+  constructor({endPoint, authorization}) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getPoints() {
+    return this._load({url: `points`})
+      .then(toJSON)
+      .then(ModelPoint.parsePoints);
+  }
+
+  getDestinations() {
+    return this._load({url: `destinations`})
+      .then(toJSON)
+      .then(ModelDestination.parseDestinations);
+  }
+
+  // createTask({task}) {
+  //   return this._load({
+  //     url: `tasks`,
+  //     method: Method.POST,
+  //     body: JSON.stringify(task),
+  //     headers: new Headers({'Content-Type': `application/json`})
+  //   })
+  //     .then(toJSON);
+  // }
+
+  updatePoint({id, data}) {
+    return this._load({
+      url: `points/${id}`,
+      method: Method.PUT,
+      body: JSON.stringify(data),
+      headers: new Headers({'Content-Type': `application/json`})
+    })
+      .then(toJSON);
+  }
+
+  deletePoint({id}) {
+    return this._load({url: `points/${id}`, method: Method.DELETE});
+  }
+
+  _load({url, method = Method.GET, body = null, headers = new Headers()}) {
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`${this._endPoint}/${url}`, {method, body, headers})
+      .then(checkStatus)
+      .catch((err) => {
+        console.error(`fetch error: ${err}`);
+        throw err;
+      });
+  }
+};
